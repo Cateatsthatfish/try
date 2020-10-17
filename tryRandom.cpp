5b59b46bb5d71bec424b5a64b70e8a4d6e3d7927
Corrine <867155532@qq.com>
2020-10-17 20:34:03 +0800

    try to generate random float in float*  v1 and v2

--- tryRandom.cpp
+++ tryRandom.cpp
@@ -9,9 +9,25 @@ const int a = 1;
 const int b = 2;
 
 int main(){
+    float * v1 = new float;
+    float * v2 = new float;
+    //float r1,r2;
+
     srand((int)time(0));
-    float r1 = a + rand()%(b-a) + rand()/double(RAND_MAX);
-    float r2 = a + rand()%(b-a) + rand()/double(RAND_MAX);
-    cout << r1 << " " << r2 <<endl;
+    for (int i = 0; i < 2; i++) 
+    {
+         v1[i] = a + rand()%(b-a) + rand()/double(RAND_MAX);
+         v2[i] = a + rand()%(b-a) + rand()/double(RAND_MAX);
+    }
+    for (int i = 0; i < 2; i++) 
+    {
+         cout << v1[i] << " ";
+         cout << v2[i] ;
+         cout << endl;
+    }
+    delete [] v1;
+    delete [] v2;
+     
+    //cout << r1 << " " << r2 <<endl;
     return 0;
 }
